25b42421ca1fc1769cbf282579ac6a04ab7885ec
My Name <my@email>
2013-12-01 08:51:01 -0700

    haha this is awesome

--- consensus.js
+++ consensus.js
@@ -54,6 +54,15 @@ var StateTransition = {
 	},
 
 	/*
+		Unapply this transition on the state object.
+		Like setting "ignoreunspent" and "ignorespent" outputs for the validator.
+	*/
+
+	unapply: function(state) {
+
+	},
+
+	/*
 		Can we apply the transition to this state (delta)?
 	*/
 	validate: function(state, validation) {
@@ -70,15 +79,23 @@ var StateTransition = {
 		//  5. this transition is invalid for some reason, so INVALID
 
 		return validation;
+	},
+
+	invalidate: function(state, validation) {
+		if (!validation) {
+			validation = new StateTransitionValidation(this)
+		}
+
+		// valid, duplicate or partial
+
+		return validation;
 	}
 }
 
 function ConsensusState(parent, transition) {
-	if (transition)
-		this.transitions = [transition]
-	else
-		this.transitions = []
+	this.transitions = []
 
+	this.untransitions = [];
 	this.parent = parent;
 	this.children = [];
 	this._retain = 0;
@@ -91,6 +108,7 @@ function ConsensusState(parent, transition) {
 		this.id = StateTransition.rand();
 		this.root = this;
 		this.shifts = {};
+		this.unshifts = {};
 	}
 
 	if (transition) {
@@ -128,6 +146,36 @@ ConsensusState.prototype = {
 		}
 		return false;
 	},
+	unshift: function(transition, validation) {
+		if (validation.state == validation.VALID) {
+			var n;
+
+			if (this.root.unshifts[transition.xor(this.id)]) {
+				n = this.root.unshifts[transition.xor(this.id)]
+			} else {
+				n = new ConsensusState(this, false);
+				// unapply all of validation's transitions
+
+				validation.untransitions.forEach(function(tr) {
+					tr.unapply(n)
+				}, this)
+
+				this.root.unshifts[transition.xor(this.id)] = n
+
+				n.retain(); // new child means retain
+			}
+
+			// retain our new state
+			n.retain();
+
+			// release ourselves
+			this.release();
+
+			return n;
+		}
+
+		return false;
+	},
 	validate: function(transition) {
 		var cur = this;
 
@@ -137,6 +185,31 @@ ConsensusState.prototype = {
 			validation = transition.validate(cur, validation);
 
 			switch (validation.state) {
+				case validation.PARTIAL:
+					// do nothing, continue up the chain
+				break;
+				case validation.VALID:
+				case validation.CONFLICT:
+				case validation.INVALID:
+				case validation.DUPLICATE:
+					return validation;
+				break;
+			}
+
+			cur = cur.parent;
+		}
+
+		return validation;
+	},
+	invalidate: function(transition) {
+		var cur = this;
+
+		var validation = false;
+
+		while(cur) {
+			validation = transition.invalidate(cur, validation)
+
+			switch(validation.state) {
 				case validation.PARTIAL:
 					// do nothing, continue up the chain
 				break;
@@ -172,7 +245,6 @@ ConsensusState.prototype = {
 				this.transitions.forEach(function(t) {
 					t.apply(child) // apply parent transition to child state
 				})
-				child.transitions = this.transitions.concat(child.transitions)
 
 				child.release();
 			}, this)
@@ -187,7 +259,7 @@ ConsensusState.prototype = {
 
 
 
-
+/*
 //////////////////////// INVENTORY STORAGE
 
 function InventoryValidation(transition) {
@@ -209,6 +281,14 @@ function InventoryTransition(name, obj) {
 		state.objs[name] = obj;
 	}
 
+	this.unapply = function(state) {
+		if (typeof state.ignoreobjs == "undefined") {
+			state.ignoreobjs = {};
+		}
+
+		state.ignoreobjs[name] = obj;
+	}
+
 	this.validate = function(state, validation) {
 		if (!validation) {
 			validation = new InventoryValidation(this)
@@ -222,52 +302,212 @@ function InventoryTransition(name, obj) {
 
 		return validation;
 	}
+
+	this.invalidate = function(state, validation) {
+		if (!validation) {
+			validation = new InventoryValidation(this)
+		}
+
+		if (state.untransitions.indexOf(this) != -1) {
+			validation.state = validation.DUPLICATE;
+		} else {
+			if (state.transitions.indexOf(this) != 1) {
+				validation.state = validation.VALID;
+			} else {
+				// otherwise, remain PARTIAL
+			}
+		}
+
+		return validation;
+	}
 }
 
 InventoryTransition.prototype = StateTransition;
 
-var inv1 = new InventoryTransition("first", "test lol") // transaction 1
-var inv2 = new InventoryTransition("second", "rofl lol") // transaction 2
-var inv3 = new InventoryTransition("third", "POOP lol") // transaction 3
 
-var root = new ConsensusState(false, false) // root state
+*/
+
+
+///////////////// UTXO Storage
+
+
+function UTXOTransition(inputs, outputs) {
+	this.id = this.rand();
+
+	this.apply = function(state) {
+		if (typeof state.spents == "undefined")
+			state.spents = {};
+		if (typeof state.unspents == "undefined")
+			state.unspents = {};
+		if (typeof state.ignorespents == "undefined")
+			state.ignorespents = {};
+		if (typeof state.ignoreunspents == "undefined")
+			state.ignoreunspents = {};
+
+		if (state.untransitions.indexOf(this) != -1) {
+			// remove this transition from untransitions
+			state.untransitions.splice(state.untransitions.indexOf(this), 1)
+
+			// remove this transitions' untransitions oh god what the fuck is happening
+			inputs.forEach(function(input) {
+				delete state.ignorespents[input];
+			}, this)
+
+			outputs.forEach(function(output) {
+				delete state.ignoreunspents[output];
+			}, this)
+		} else {
+			inputs.forEach(function(input) {
+				state.spents[input] = this;
+			}, this)
+
+			outputs.forEach(function(output) {
+				state.unspents[output] = this;
+			}, this)
+
+			state.transitions.unshift(this)
+		}
+	}
+
+	this.unapply = function(state) {
+		if (state.untransitions.indexOf(this) != -1)
+			return; // don't unapply twice (happens in long reorgs)
+
+		if (typeof state.spents == "undefined")
+			state.spents = {};
+		if (typeof state.unspents == "undefined")
+			state.unspents = {};
+		if (typeof state.ignorespents == "undefined")
+			state.ignorespents = {};
+		if (typeof state.ignoreunspents == "undefined")
+			state.ignoreunspents = {};
+
+		// set ignoreunspents and ignorespents
+		inputs.forEach(function(input) {
+			state.ignorespents[input] = this;
+		}, this)
+
+		outputs.forEach(function(output) {
+			state.ignoreunspents[output] = this;
+		}, this)
+
+		state.id = this.xor(state.id)
+
+		state.untransitions.unshift(this)
+	}
+
+	this.validate = function(state, validation) {
+		if (!validation) {
+			validation = new StateTransitionValidation(this)
+			validation.inputsMissing = inputs.slice(0); // keep track of inputs we're missing
+			validation.ignoreSpents = {};
+			validation.ignoreUnspents = {};
+			validation.untransitions = [];
+		}
+
+		validation.untransitions = validation.untransitions.concat(state.untransitions);
+
+		if (state.ignoreunspents) {
+			for (var sp in state.ignoreunspents) {
+				validation.ignoreUnspents[sp] = state.ignoreunspents[sp]
+			}
+		}
+
+		if (state.ignorespents) {
+			for (var usp in state.ignorespents) {
+				validation.ignoreSpents[usp] = state.ignorespents[usp]
+			}
+		}
+
+		if (state.transitions.indexOf(this) != -1) {
+			if (validation.untransitions.indexOf(this) != -1) {
+				// remove untransition
+				validation.untransitions.splice(validation.untransitions.indexOf(this), 1)
+			} else {
+				// this state already performs our transition
+				validation.state = validation.DUPLICATE;
+
+				return validation;
+			}
+		} else {
+			inputs.forEach(function(input) {
+				// we shouldn't find our input being spent by another transition
+				if (typeof state.spents[input] != "undefined") {
+					if (typeof validation.ignoreSpents[input] != "undefined") {
+						delete validation.ignoreSpents[input]
+					} else {
+						// someone spent this!
+						validation.state = validation.CONFLICT;
+						validation.conflictTx = state.spents[input];
+
+						return validation;
+					}
+				}
+			})
+
+			validation.inputsMissing.forEach(function(missingInput) {
+				if (typeof state.unspents[missingInput] != "undefined") {
+					if (typeof validation.ignoreUnspents[missingInput] != "undefined") {
+						delete validation.ignoreUnspents[missingInput]
+					} else {
+						validation.inputsMissing.splice(validation.inputsMissing.indexOf(missingInput), 1) // remove the missingInput, we found it
+					}
+				}
+			})
+		}
+
+		if (validation.inputsMissing.length == 0) {
+			validation.state = validation.VALID;
+		}
+
+		return validation;
+	}
+
+	this.invalidate = function(state, validation) {
+		if (!validation) {
+			validation = new StateTransitionValidation(this)
+			validation.base = state;
+			validation.untransitions = [this]; // array of transitions we intend to remove
+			validation.outputsSeeking = outputs.slice(0)
+		}
+
+		if (state.transitions.indexOf(this) != -1) {
+			// found the transition
+			validation.state = validation.VALID;
+		}
+
+		validation.outputsSeeking.forEach(function(output) {
+			if (typeof state.spents[output] != "undefined") {
+				validation.outputsSeeking.splice(validation.outputsSeeking.indexOf(output), 1) // we're no longer worried about this output being spent
+
+				// validate the removal of this
+				var sub = state.spents[output].invalidate(validation.base, false)
+
+				// merge its untransitions with ours
+				validation.untransitions = validation.untransitions.concat(sub.untransitions)
+			}
+		})
+
+		return validation;
+	}
+}
+
+UTXOTransition.prototype = StateTransition;
+
+var root = new ConsensusState(false, false);
+//root.retain()
+root.spents = {}
+root.unspents = {}
+var tx1 = new UTXOTransition([], ["derp"])
+var tx2 = new UTXOTransition(["derp"], ["herp", "slurp"])
+var tx3 = new UTXOTransition(["herp"], ["lerp"])
+var tx4 = new UTXOTransition(["slurp"], ["fag"])
+
 var node1 = root;
-var node2 = root; // same state
-var node3 = root; // same state
+node1.retain();
 
-node1.retain() // node 1 retains state
-node2.retain() // node 2 retains state
-node3.retain() // node 3 retains state
+node1 = node1.shift(tx1, node1.validate(tx1))
+node1 = node1.shift(tx2, node1.validate(tx2))
+node1 = node1.shift(tx3, node1.validate(tx3))
 
-var val1 = node1.validate(inv1)
-node1 = node1.shift(inv1, val1)
-
-var val2 = node2.validate(inv2)
-node2 = node2.shift(inv2, val2)
-
-var val3 = node1.validate(inv2)
-node1 = node1.shift(inv2, val3)
-
-var val4 = node2.validate(inv1)
-node2 = node2.shift(inv1, val4)
-
-var val5 = node3.validate(inv2)
-node3 = node3.shift(inv2, val5)
-
-var val6 = node3.validate(inv3)
-node3 = node3.shift(inv3, val6)
-
-var val7 = node3.validate(inv1)
-node3 = node3.shift(inv1, val7)
-
-var val8 = node1.validate(inv3)
-node1 = node1.shift(inv3, val8)
-
-var val9 = node2.validate(inv3)
-node2 = node2.shift(inv3, val9)
-
-console.log(node1)
-console.log(node2)
-console.log(node3)
-console.log("root: ")
-console.log(root)
+node1 = node1.unshift(tx2, node1.invalidate(tx2))
