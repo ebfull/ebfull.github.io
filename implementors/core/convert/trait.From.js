e8441034ff57029bb7efc662c50fd91f94d02e3b
ebfull <ewillbefull@gmail.com>
2015-07-31 03:53:37 -0600

    Added pcap crate documentation

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pcap'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/str/struct.Utf8Error.html' title='core::str::Utf8Error'>Utf8Error</a>&gt; for <a class='enum' href='pcap/enum.Error.html' title='pcap::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
