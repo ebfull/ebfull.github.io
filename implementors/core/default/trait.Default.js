5295efe090b188e532456d41afada5727ad011c0
ebfull <ewillbefull@gmail.com>
2015-08-06 11:38:46 -0600

    Updated pcap docs with PacketHeader

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pcap'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='pcap/struct.Timeval.html' title='pcap::Timeval'>Struct_timeval</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
