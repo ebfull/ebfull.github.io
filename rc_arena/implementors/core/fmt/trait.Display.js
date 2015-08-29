9341e36653c3d8756586a080342cc03b2709ac33
ebfull <ewillbefull@gmail.com>
2015-08-29 17:37:30 -0600

    Reorganized website

---
+++ rc_arena/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rc_arena'] = ["impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='rc_arena/struct.Rc.html' title='rc_arena::Rc'>Rc</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ rc_arena/rc_arena/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Arena","A typed arena that provides reference-counted pointers to its underlying objects."],["Rc","A reference counted pointer to an object that lives in an arena."]]});
