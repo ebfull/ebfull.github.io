adf02ed4ad6772b673f4ab8fc4ee79cb106d33c2
ebfull <ewillbefull@gmail.com>
2015-12-13 17:36:06 -0700

    Update pcap docs

--- pcap/implementors/core/fmt/trait.Display.js
+++ pcap/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pcap'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='pcap/enum.Error.html' title='pcap::Error'>Error</a>",];
+implementors['pcap'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='pcap/enum.Error.html' title='pcap::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
