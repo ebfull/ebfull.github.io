f136c0ad18789568686921f5698dc2714120828f
ebfull <ewillbefull@gmail.com>
2016-04-14 03:00:27 -0600

    Updated pcap documentation.

---
+++ pcap/implementors/std/sys/ext/io/trait.AsRawFd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pcap'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/sys/ext/io/trait.AsRawFd.html' title='std::sys::ext::io::AsRawFd'>AsRawFd</a> for <a class='struct' href='pcap/struct.Capture.html' title='pcap::Capture'>Capture</a>&lt;<a class='enum' href='pcap/enum.Active.html' title='pcap::Active'>Active</a>&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
